2ff795f0f085c138867dc9fa2fc2844085f1d5e8
Mazhiwei <mzw1004@126.com>
2014-10-06 17:26:26 +0800

    Update

--- app/src/main/java/com/mzw/jobinformation/MainActivity.java
+++ app/src/main/java/com/mzw/jobinformation/MainActivity.java
@@ -1,33 +1,44 @@
 package com.mzw.jobinformation;
 
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewConfiguration;
+import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     private PagerSlidingTabStrip tabs;
+
+    private JobInfoFragment jobInfoFragment;
+
+    private JobFairFragment jobFairFragment;
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
@@ -35,50 +46,40 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ListView> viewList = new ArrayList<ListView>();
-        LayoutInflater inflater = getLayoutInflater();
-
-        View view1 = inflater.inflate(R.layout.job_info_list, null);
-        View view2 = inflater.inflate(R.layout.job_info_list, null);
-        ListView listview1 = (ListView) view1.findViewById(R.id.listview);
-        ListView listview2 = (ListView) view2.findViewById(R.id.listview);
-
-        SimpleAdapter simpleAdapterJobInfo= new SimpleAdapter(this, getInfoData(),
-                R.layout.job_info, new String[] {"date", "title", "comp"},
-                new int[] {R.id.date, R.id.title, R.id.comp});
-
-        SimpleAdapter simpleAdapterJobFair= new SimpleAdapter(this, getFairData(),
-                R.layout.job_fair, new String[] {"date", "title", "comp", "time", "place"},
-                new int[] {R.id.date, R.id.title, R.id.comp, R.id.time, R.id.place});
-
-        listview1.setAdapter(simpleAdapterJobInfo);
-        listview2.setAdapter(simpleAdapterJobFair);
-
-        listview1.setOnItemClickListener(new JobInfoListener());
-
-        viewList.add(listview1);
-        viewList.add(listview2);
-
         // Initialize the ViewPager and set an adapter
         ViewPager pager = (ViewPager)findViewById(R.id.pager);
-        pager.setAdapter(new ViewAdapter(viewList));
+        pager.setAdapter(new ViewAdapter(getSupportFragmentManager()));
 
         // Bind the tabs to the ViewPager
         tabs = (PagerSlidingTabStrip)findViewById(R.id.tabs);
+        setTabsValue();
         tabs.setViewPager(pager);
 
-        tabs.setIndicatorColor(0xFF228b22);
-        tabs.setTextColor(0xFF228b22);
-        getActionBar().setBackgroundDrawable(new ColorDrawable(0xFF228b22));
+        ActionBar actionBar = this.getActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFF228b22));
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.actionbar);
     }
 
-    class ViewAdapter extends PagerAdapter {
+    private void setTabsValue() {
+        // 设置Tab是自动填充满屏幕的
+        tabs.setShouldExpand(true);
+        // 设置Tab的分割线是透明的
+        tabs.setDividerColor(Color.TRANSPARENT);
+        // 取消点击Tab时的背景色
+        tabs.setTabBackground(0);
+        tabs.setIndicatorColor(0xFF228b22);
+        tabs.setTextColor(0xFF228b22);
+    }
+
+    class ViewAdapter extends FragmentPagerAdapter {
 
         private final String[] TITLES = { "招聘信息", "招聘会" };
-        private final ArrayList<ListView> viewList;
 
-        ViewAdapter(ArrayList<ListView> vl) {
-            viewList = vl;
+        public ViewAdapter(FragmentManager fm) {
+            super(fm);
         }
 
         @Override
@@ -87,65 +88,40 @@ public class MainActivity extends ActionBarActivity {
         }
 
         @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    if (jobInfoFragment == null) {
+                        jobInfoFragment = new JobInfoFragment();
+                    }
+                    return jobInfoFragment;
+                case 1:
+                    if (jobFairFragment == null) {
+                        jobFairFragment = new JobFairFragment();
+                    }
+                    return jobFairFragment;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
         public int getCount() {
             return TITLES.length;
         }
-
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return view == o;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            ListView v = viewList.get(position);
-            container.addView(v, position);
-
-            return v;
-        }
     }
 
-    private List<Map<String, Object>> getInfoData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        Map<String, Object> map;
-
-        for (int i = 1; i < 15; i++) {
-            map = new HashMap<String, Object>();
-            map.put("date", "2014-10-" + i);
-            map.put("title", "job" + i);
-            map.put("comp", "Comp" + i);
-            list.add(map);
-        }
-
-        return list;
-    }
-
-    private List<Map<String, Object>> getFairData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        Map<String, Object> map;
-
-        for (int i = 0; i < 15; i++) {
-            map = new HashMap<String, Object>();
-            map.put("date", "2014-10-" + i);
-            map.put("title", "job" + i);
-            map.put("comp", "Comp" + i);
-            map.put("time", i);
-            map.put("place", "Room" + i);
-            list.add(map);
-        }
-
-        return list;
-    }
-
-    class JobInfoListener implements AdapterView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            System.out.println(i);
-            Intent intent = new Intent();
-            intent.setClass(MainActivity.this, JobInfoActivity.class);
-            intent.setData(Uri.parse("" + i));
-            startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+            case R.id.collection:
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, CollectionActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
@@ -156,15 +132,31 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    private void setOverflowShowingAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class
+                    .getDeclaredField("sHasPermanentMenuKey");
+            menuKeyField.setAccessible(true);
+            menuKeyField.setBoolean(config, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
